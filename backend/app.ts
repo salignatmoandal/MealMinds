46c57e31333fed8d600b89744ad041c3abe941a2
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2025-01-18 20:03:40 +0100

    fix

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,16 @@
 import { Hono } from "hono";
+import ingredientRoutes from './routes/ingredientRoutes';
 
 const app = new Hono();
 
-app.get('/', (c) => {
-    return c.text('Hello World');
+app.get('/test', (c) => {
+    return c.json({ message: 'Hello Test' });
 });
+
+app.get('/users', (c) => {
+    return c.json({ message: 'Hello Users' });
+});
+
+app.use(ingredientRoutes);
 
 export default app;
